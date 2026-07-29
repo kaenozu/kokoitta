e45ae6c5c99f72e7ea398647c56b8590db5e518b
kaenozu <106388957+kaenozu@users.noreply.github.com>
2026-07-29 23:41:22 +0900

    feat: add test/widget_test.dart

---
+++ test/widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:kokoitta_app/main.dart';
+
+void main() {
+  testWidgets('ホームに地図と旅行タブを表示する', (tester) async {
+    await tester.pumpWidget(const KokoittaApp());
+    expect(find.text('ここいった'), findsOneWidget);
+    expect(find.text('地図'), findsOneWidget);
+    expect(find.text('旅行'), findsOneWidget);
+    expect(find.text('写真を追加'), findsOneWidget);
+  });
+}
+
