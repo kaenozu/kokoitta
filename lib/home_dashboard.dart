c3ece799c3c8b4329351e31d9b114283760d9cc3
kaenozu <106388957+kaenozu@users.noreply.github.com>
2026-08-05 19:02:21 +0900

    refactor: import design primitives directly

--- lib/home_dashboard.dart
+++ lib/home_dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'app_theme.dart';
+import 'design/kokoitta_components.dart';
+import 'design/kokoitta_design_system.dart';
 import 'offline_japan_map.dart';
 
 @immutable
