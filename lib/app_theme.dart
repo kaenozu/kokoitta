0de1c155c6ebe1b086bb33a9fbcf0bd2600450e1
kaenozu <106388957+kaenozu@users.noreply.github.com>
2026-08-06 14:04:57 +0900

    feat: export settings and backup view

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -7,6 +7,7 @@ export 'design/kokoitta_design_system.dart';
 export 'home_dashboard.dart';
 export 'import_state_contract.dart';
 export 'prefecture_state_list_tile.dart';
+export 'settings_backup_view.dart';
 export 'trip_detail_view.dart';
 export 'trip_list_view.dart';
 
